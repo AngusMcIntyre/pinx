2a9a240d699d160008e93aec16a16bbcce31dfca
gusdor@gmail.com <gusdor@gmail.com@a8f98f35-7e29-5427-5690-6b4ffb64ed68>
2012-07-19 14:16:47 +0000

    First release and release materials

--- gen/gus/apps/pinsafe/R.java
+++ gen/gus/apps/pinsafe/R.java
@@ -18,6 +18,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_pinsafe_launcher=0x7f020002;
     }
     public static final class id {
         public static final int editText_pinlabel=0x7f080000;
@@ -41,20 +42,19 @@ public final class R {
         public static final int activity_pins=0x7f070001;
     }
     public static final class string {
-        public static final int add_pin=0x7f050004;
-        public static final int add_pin_addhint=0x7f05000a;
-        public static final int add_pin_valuehint=0x7f05000b;
+        public static final int add_pin=0x7f050003;
+        public static final int add_pin_addhint=0x7f050009;
+        public static final int add_pin_valuehint=0x7f05000a;
         public static final int app_name=0x7f050000;
-        public static final int delete_all_pins=0x7f050009;
-        public static final int enter_master_pin=0x7f050005;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_deletepin=0x7f05000c;
-        public static final int menu_settings=0x7f050002;
-        public static final int menu_title_pinoptions=0x7f05000d;
-        public static final int pin_restriction_tip=0x7f050006;
-        public static final int title_activity_add_pin=0x7f050008;
-        public static final int title_activity_main=0x7f050007;
-        public static final int title_activity_master_pin=0x7f050003;
+        public static final int delete_all_pins=0x7f050008;
+        public static final int enter_master_pin=0x7f050004;
+        public static final int menu_deletepin=0x7f05000b;
+        public static final int menu_settings=0x7f050001;
+        public static final int menu_title_pinoptions=0x7f05000c;
+        public static final int pin_restriction_tip=0x7f050005;
+        public static final int title_activity_add_pin=0x7f050007;
+        public static final int title_activity_main=0x7f050006;
+        public static final int title_activity_master_pin=0x7f050002;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
