02792e8905d7b732a9af143b509a0c77ba8c5ada
gusdor@gmail.com <gusdor@gmail.com@a8f98f35-7e29-5427-5690-6b4ffb64ed68>
2013-01-20 17:17:00 +0000

    +Added first version of keypad widget

--- gen/gus/apps/pinsafe/R.java
+++ gen/gus/apps/pinsafe/R.java
@@ -11,9 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int keypad_button_width=0x7f050005;
+        public static final int keypadbutton_height=0x7f050004;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
+        public static final int text_small=0x7f050003;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
@@ -21,42 +24,70 @@ public final class R {
         public static final int ic_pinsafe_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int editText_pinlabel=0x7f080000;
-        public static final int editText_pinvalue=0x7f080001;
-        public static final int edittext_masterpin=0x7f080002;
-        public static final int listview_pins=0x7f080004;
-        public static final int menu_addpin=0x7f080007;
-        public static final int menu_cancel=0x7f080006;
-        public static final int menu_deleteall=0x7f080008;
-        public static final int menu_done=0x7f080005;
-        public static final int textView_tip=0x7f080003;
+        public static final int editText_pinlabel=0x7f090000;
+        public static final int editText_pinvalue=0x7f090001;
+        public static final int edittext_masterpin=0x7f090002;
+        public static final int keypad_clear=0x7f090010;
+        public static final int keypad_eight=0x7f09000d;
+        public static final int keypad_five=0x7f09000a;
+        public static final int keypad_four=0x7f090009;
+        public static final int keypad_nine=0x7f09000e;
+        public static final int keypad_one=0x7f090006;
+        public static final int keypad_seven=0x7f09000c;
+        public static final int keypad_six=0x7f09000b;
+        public static final int keypad_three=0x7f090008;
+        public static final int keypad_two=0x7f090007;
+        public static final int keypad_zero=0x7f09000f;
+        public static final int keypadwidget_title=0x7f090005;
+        public static final int listview_pins=0x7f090004;
+        public static final int menu_addpin=0x7f090013;
+        public static final int menu_cancel=0x7f090012;
+        public static final int menu_deleteall=0x7f090014;
+        public static final int menu_done=0x7f090011;
+        public static final int textView_tip=0x7f090003;
     }
     public static final class layout {
         public static final int activity_add_pin=0x7f030000;
         public static final int activity_master_pin=0x7f030001;
         public static final int activity_pins=0x7f030002;
         public static final int pins_listview=0x7f030003;
+        public static final int widget_keypad_tall=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_addpin=0x7f070000;
-        public static final int activity_pins=0x7f070001;
+        public static final int activity_addpin=0x7f080000;
+        public static final int activity_pins=0x7f080001;
     }
     public static final class string {
-        public static final int add_pin=0x7f050003;
-        public static final int add_pin_addhint=0x7f050009;
-        public static final int add_pin_valuehint=0x7f05000a;
-        public static final int app_name=0x7f050000;
-        public static final int delete_all_pins=0x7f050008;
-        public static final int enter_master_pin=0x7f050004;
-        public static final int menu_deletepin=0x7f05000b;
-        public static final int menu_settings=0x7f050001;
-        public static final int menu_title_pinoptions=0x7f05000c;
-        public static final int pin_restriction_tip=0x7f050005;
-        public static final int title_activity_add_pin=0x7f050007;
-        public static final int title_activity_main=0x7f050006;
-        public static final int title_activity_master_pin=0x7f050002;
+        public static final int add_pin=0x7f060003;
+        public static final int add_pin_addhint=0x7f060009;
+        public static final int add_pin_valuehint=0x7f06000a;
+        public static final int app_name=0x7f060000;
+        public static final int delete_all_pins=0x7f060008;
+        public static final int enter_master_pin=0x7f060004;
+        public static final int keypad_clear=0x7f06000d;
+        public static final int keypad_eight=0x7f060015;
+        public static final int keypad_five=0x7f060012;
+        public static final int keypad_four=0x7f060011;
+        public static final int keypad_nine=0x7f060016;
+        public static final int keypad_one=0x7f06000e;
+        public static final int keypad_seven=0x7f060014;
+        public static final int keypad_six=0x7f060013;
+        public static final int keypad_three=0x7f060010;
+        public static final int keypad_two=0x7f06000f;
+        public static final int keypad_zero=0x7f060017;
+        public static final int menu_deletepin=0x7f06000b;
+        public static final int menu_settings=0x7f060001;
+        public static final int menu_title_pinoptions=0x7f06000c;
+        public static final int pin_restriction_tip=0x7f060005;
+        public static final int title_activity_add_pin=0x7f060007;
+        public static final int title_activity_main=0x7f060006;
+        public static final int title_activity_master_pin=0x7f060002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+        public static final int style_keypad_button=0x7f070001;
+    }
+    public static final class xml {
+        public static final int keypad_widget_info=0x7f040000;
     }
 }
